7ed2242b4140cd439fefb044ff097a990760d20e
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2021-09-04 01:13:21 +0530

    Update signIn.js

--- Js/signIn.js
+++ Js/signIn.js
@@ -1,61 +1,66 @@
-var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
+function SigninPage() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
 
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
+  const IsInvalid = password === "" || emailAddress === "";
 
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
+  function handleSubmit(event) {
+    event.preventDefault();
 
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        setEmailAddress("");
+        setPassword("");
+        history.push("/browse");
+      })
+      .catch((error) => setError(error.message));
+  }
 
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
+  return (
+    <>
+      <HeaderWrapper className="header-wrapper-home">
+        <NavBar className="navbar-signin">
+          <Logo />
+        </NavBar>
+        <SignFormWrapper>
+          <SignFormBase onSubmit={handleSubmit} method="POST">
+            <Warning>NOT official Netflix</Warning>
+            <SignFormTitle>Sign In</SignFormTitle>
+            {error ? <SignFormError>{error}</SignFormError> : null}
+            <SignFormInput
+              type="text"
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <SignFormInput
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <SignFormButton disabled={IsInvalid}>Sign In</SignFormButton>
+            <SignFormText>
+              New to Netflix?
+              <SignFormLink href="/signup">Sign up now.</SignFormLink>
+            </SignFormText>
+            <SignFormCaptcha>
+              This page is protected by Google reCAPTCHA to ensure you are not a
+              bot.
+            </SignFormCaptcha>
+          </SignFormBase>
+        </SignFormWrapper>
+      </HeaderWrapper>
+      <FooterCompound />
+    </>
+  );
+}
 
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
-
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
-
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
-
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
-
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})
+export default SigninPage;
