1ca0ee0fe6102ac203f4a359d3c4314ad57270e4
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2021-09-04 01:17:24 +0530

    Update browsePage.js

--- Js/browsePage.js
+++ Js/browsePage.js
@@ -1,4 +1,46 @@
-var playbtn = document.getElementsByClassName("play-button");
+function BrowsePage() {
+  let { series } = useContent("series");
+  series = [
+    {
+      title: "Documentaries",
+      data: series.filter((item) => item.genre === "documentaries"),
+    },
+    {
+      title: "Comedies",
+      data: series.filter((item) => item.genre === "comedies"),
+    },
+    {
+      title: "Children",
+      data: series.filter((item) => item.genre === "children"),
+    },
+    { title: "Crime", data: series.filter((item) => item.genre === "crime") },
+    {
+      title: "Feel-Good",
+      data: series.filter((item) => item.genre === "feel-good"),
+    },
+  ];
+
+  let { films } = useContent("films");
+  films = [
+    { title: "Drama", data: films.filter((item) => item.genre === "drama") },
+    {
+      title: "Thriller",
+      data: films.filter((item) => item.genre === "thriller"),
+    },
+    {
+      title: "Children",
+      data: films.filter((item) => item.genre === "children"),
+    },
+    {
+      title: "Suspense",
+      data: films.filter((item) => item.genre === "suspense"),
+    },
+    {
+      title: "Romance",
+      data: films.filter((item) => item.genre === "romance"),
+    },
+  ];
+  var playbtn = document.getElementsByClassName("play-button");
 var featureCard = document.getElementByClassName("feature-movie");
 
 
@@ -8,83 +50,7 @@ playbtn[i].addEventListener('click', function(){
   videoOverlay.className = "player-overlay";
   featureCard.appendChild(videoOverlay);
   
-})
-var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
+}) 
 
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})
-var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
-
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})
-var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
-
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})
-var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
-
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})
-var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
-
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})
-var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
-
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})
-var playbtn = document.getElementsByClassName("play-button");
-var featureCard = document.getElementByClassName("feature-movie");
-
-
-playbtn[i].addEventListener('click', function(){
-  var videoOverlay = document.createElement('div');
-
-  videoOverlay.className = "player-overlay";
-  featureCard.appendChild(videoOverlay);
-  
-})
-
+}
 
