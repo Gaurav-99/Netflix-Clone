e01ace720c45e4689833335972bb8a30b3704b87
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2021-09-04 01:12:54 +0530

    Create signUp.js

---
+++ Js/signUp.js
@@ -0,0 +1,80 @@
+function SignupPage() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [firstName, setFirstName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const IsInvalid = password === "" || emailAddress === "" || firstName === "";
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+          })
+          .then(() => {
+            setFirstName("");
+            setEmailAddress("");
+            setPassword("");
+            history.push("/browse");
+          })
+      )
+      .catch((error) => setError(error.message));
+  }
+
+  return (
+    <>
+      <HeaderWrapper className="header-wrapper-home">
+        <NavBar className="navbar-signin">
+          <Logo />
+        </NavBar>
+        <SignFormWrapper>
+          <SignFormBase onSubmit={handleSubmit} method="POST">
+            <Warning>NOT official Netflix</Warning>
+            <SignFormTitle>Sign Up</SignFormTitle>
+            {error ? <SignFormError>{error}</SignFormError> : null}
+            <SignFormInput
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <SignFormInput
+              type="text"
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <SignFormInput
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <SignFormButton disabled={IsInvalid}>Sign Up</SignFormButton>
+            <SignFormText>
+              Already a user?
+              <SignFormLink href="/signin">Sign in now.</SignFormLink>
+            </SignFormText>
+            <SignFormCaptcha>
+              This page is protected by Google reCAPTCHA to ensure you are not a
+              bot.
+            </SignFormCaptcha>
+          </SignFormBase>
+        </SignFormWrapper>
+      </HeaderWrapper>
+      <FooterCompound />
+    </>
+  );
+}
+
+export default SignupPage;
