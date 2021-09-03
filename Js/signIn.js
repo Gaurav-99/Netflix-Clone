499ff2c83f4d48c8b002a4c3235e145d79f14896
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2021-09-04 01:11:02 +0530

    Create signIn.js

---
+++ Js/signIn.js
@@ -0,0 +1,61 @@
+var playbtn = document.getElementsByClassName("play-button");
+var featureCard = document.getElementByClassName("feature-movie");
+
+
+playbtn[i].addEventListener('click', function(){
+  var videoOverlay = document.createElement('div');
+
+  videoOverlay.className = "player-overlay";
+  featureCard.appendChild(videoOverlay);
+  
+})var playbtn = document.getElementsByClassName("play-button");
+var featureCard = document.getElementByClassName("feature-movie");
+
+
+playbtn[i].addEventListener('click', function(){
+  var videoOverlay = document.createElement('div');
+
+  videoOverlay.className = "player-overlay";
+  featureCard.appendChild(videoOverlay);
+  
+})var playbtn = document.getElementsByClassName("play-button");
+var featureCard = document.getElementByClassName("feature-movie");
+
+
+playbtn[i].addEventListener('click', function(){
+  var videoOverlay = document.createElement('div');
+
+  videoOverlay.className = "player-overlay";
+  featureCard.appendChild(videoOverlay);
+  
+})var playbtn = document.getElementsByClassName("play-button");
+var featureCard = document.getElementByClassName("feature-movie");
+
+
+playbtn[i].addEventListener('click', function(){
+  var videoOverlay = document.createElement('div');
+
+  videoOverlay.className = "player-overlay";
+  featureCard.appendChild(videoOverlay);
+  
+})var playbtn = document.getElementsByClassName("play-button");
+var featureCard = document.getElementByClassName("feature-movie");
+
+
+playbtn[i].addEventListener('click', function(){
+  var videoOverlay = document.createElement('div');
+
+  videoOverlay.className = "player-overlay";
+  featureCard.appendChild(videoOverlay);
+  
+})var playbtn = document.getElementsByClassName("play-button");
+var featureCard = document.getElementByClassName("feature-movie");
+
+
+playbtn[i].addEventListener('click', function(){
+  var videoOverlay = document.createElement('div');
+
+  videoOverlay.className = "player-overlay";
+  featureCard.appendChild(videoOverlay);
+  
+})
